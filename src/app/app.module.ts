bf3fe76d57ed33cada95c4860090a0f4def5279e
Talmoudi <Amani.Talmoudi@FISGLOBAL.COM>
2022-05-18 12:22:09 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxCsvParser, NgxCSVParserError, NgxCsvParserModule } from 'ngx-csv-parser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { NewJoinersComponent } from './new-joiners/new-joiners.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FileUploadComponent,
+    NewJoinersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,NgxCsvParserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
