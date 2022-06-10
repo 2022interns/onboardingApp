02fbad26316b1e8d72435a4e6eed582702f3fd34
skanderbac <skanderbac@users.noreply.github.com>
2022-06-10 09:59:45 +0100

    getnewjoiners from api

--- src/app/new-joiners/new-joiners.component.ts
+++ src/app/new-joiners/new-joiners.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IgxFilterOptions } from 'igniteui-angular';
+import {ApiService} from "../services/api.service";
 @Component({
   selector: 'app-new-joiners',
   templateUrl: './new-joiners.component.html',
@@ -9,8 +10,13 @@ export class NewJoinersComponent  {
 
   csvRecords: any;
   header: boolean = true;
+  newjoiners: any;
 
-  constructor() {
+  constructor(private serviceapi: ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.serviceapi.getNewjoiners().subscribe((result)=>this.newjoiners=result);
   }
 
   @ViewChild('fileImportInput') fileImportInput: any;
@@ -33,64 +39,7 @@ export class NewJoinersComponent  {
   } */
 
 
-  public newjoiners = [
-    {
-      isFavorite: false,
-      name: 'Terrance Orta',
-      phone: '770-504-2217',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: true,
-      name: 'Richard Mahoney',
-      phone: '423-676-2869',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/1.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: false,
-      name: 'Donna Price',
-      phone: '859-496-2817',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/50.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: false,
-      name: 'Lisa Landers',
-      phone: '901-747-3428',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/3.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: false,
-      name: 'Terrance Orta',
-      phone: '770-504-2217',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: false,
-      name: 'Terrance Orta',
-      phone: '770-504-2217',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    },
-    {
-      isFavorite: true,
-      name: 'Dorothy H. Spencer',
-      phone: '573-394-9254',
-      photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/67.jpg',
-      email: 'ameni@gmail.com',
-      interest: 'java',
-    }
-  ];
+
 
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,6 +8,7 @@ import * as MicrosoftGraph from "@microsoft/microsoft-graph-types";
 export class ApiService {
   url = 'http://localhost:3000/';
   constructor(private http: HttpClient) { }
+
   getCalendar(model?: MicrosoftGraph.Event){
     let storage = JSON.parse(<string>localStorage.getItem('fd8224fb-1681-459b-9de7-b4b865020f65.88f58169-ed46-4a73-8f4c-7efff9f3e4fa-login.windows.net-accesstoken-92bfacc0-fa7d-4b36-91e8-f4f1a5e84c80-88f58169-ed46-4a73-8f4c-7efff9f3e4fa-calendars.readwrite mailboxsettings.read openid profile user.read email'));
     const token =  storage.secret;
@@ -32,5 +33,9 @@ export class ApiService {
       "token": token
     };
     return this.http.post<any>(this.url,findEvent);
+  }
+
+  getNewjoiners(){
+    return this.http.get<any>(this.url+'newjoiners');
   }
 }
