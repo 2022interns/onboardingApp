f35eb9f38060ceebf94ae720f0417d9408c2a161
skanderbac <skanderbac@users.noreply.github.com>
2022-06-29 10:27:26 +0100

    file upload

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -27,24 +27,24 @@ export class FileUploadComponent  {
       let formData = new FormData();
       for (var i = 0; i < this.uploadedFiles.length; i++) {
           formData.append("file", this.uploadedFiles[i], this.uploadedFiles[i].name);
-         
+
       }
-      this.http.post('http://localhost:3000/mentors', formData)
+      this.http.post('http://localhost:3000/users/mentors', formData)
           .subscribe((response) => {
-              console.log('response received is ', response);
+              alert('mentors added! ');
           })
   }
   upload1() {
     let formData = new FormData();
     for (var i = 0; i < this.uploadedFiles1.length; i++) {
         formData.append("file", this.uploadedFiles1[i], this.uploadedFiles1[i].name);
-       
+
     }
-    this.http.post('http://localhost:3000/newjoiners', formData)
+    this.http.post('http://localhost:3000/users/newjoiners', formData)
         .subscribe((response) => {
-            console.log('response received is ', response);
+            alert('new joiners added!');
         })
 }
- 
+
 
 }
